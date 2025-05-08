4cceaba67e08f06eadeddb1f363808c5cadba906
ItzCobaltboy <pathanaffan62@gmail.com>
2025-05-08 15:21:53 +0530

    Setted up database handling using PostgreSQL

---
+++ app/telemetry.py
@@ -0,0 +1,97 @@
+# Handler for Telemetry events
+import os
+import yaml
+import psycopg2
+from app.logger import log_debug, log_error, log_info, log_warning
+
+# Load config
+def load_config():
+    config_path = os.path.join(os.path.dirname(__file__), '..', 'config', 'config.yaml')
+    with open(config_path, 'r') as f:
+        return yaml.safe_load(f)
+
+
+config = load_config()
+
+# Load parameters
+DB_USER = config["database"]["db_username"]
+DB_PASSWORD = config["database"]["db_password"]
+DB_HOST = config["database"]["db_host"]
+DB_PORT = config["database"]["db_port"]
+DB_NAME = config["database"]["db_name"]
+
+# Connect to database
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT)
+cur = conn.cursor()
+log_info("Connected to the database successfully at IP: " + DB_HOST + " and Port: " + str(DB_PORT))
+
+# Create Master Table for Launch events, tagging all the individual launch events
+try:
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS telemetry_events (
+        id SERIAL PRIMARY KEY,
+        city_map_file TEXT,
+        trashcan_data_file TEXT,
+        traffic_data_file TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+''')
+    conn.commit()
+    log_info("Master events table connected successfully.")
+except Exception as e:
+    log_error(f"Error creating master_events table: {e}")
+    conn.rollback()
+
+
+# Create Table for recording pings
+# will record the pings from the clients
+
+try:
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS pings (
+        id SERIAL PRIMARY KEY,
+        client_id TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        endpoint TEXT,
+        event_data TEXT,
+        status_code INTEGER    )
+''')
+    conn.commit()
+    log_info("Pings table connected successfully.")
+except Exception as e:
+    log_error(f"Error creating pings table: {e}")
+    conn.rollback()
+
+
+def db_log_launch_telemetry(city_map_file, trashcan_data_file, traffic_data_file):
+    """
+    Log telemetry data for the launch event.
+    """
+    try:
+        # Insert telemetry data into the database
+        cur.execute('''
+            INSERT INTO telemetry_events (city_map_file, trashcan_data_file, traffic_data_file)
+            VALUES (%s, %s, %s)
+        ''', (city_map_file, trashcan_data_file, traffic_data_file))
+        conn.commit()
+        log_info(f"New City map launched: {city_map_file}, {trashcan_data_file}, {traffic_data_file}")
+    except Exception as e:
+        log_error(f"Error logging telemetry data: {e}")
+        conn.rollback()  # Rollback in case of error
+
+def db_log_ping(client_id, endpoint, event_data, status_code):
+    '''
+    Log telemetry data for the ping event.
+    
+    '''
+    try:
+        # Insert telemetry data into the database
+        cur.execute('''
+            INSERT INTO pings (client_id, endpoint, event_data, status_code)
+            VALUES (%s, %s, %s, %s)
+        ''', (client_id, endpoint, event_data, status_code))
+        conn.commit()
+        log_info(f"New ping event logged: {client_id}, {endpoint}, {event_data}, {status_code}")
+    except Exception as e:
+        log_error(f"Error logging ping data: {e}")
+        conn.rollback()  # Rollback in case of error
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, status, HTTPException
 import os
 import yaml
 import shutil
 from app.logger import log_info, log_warning, log_error, log_debug
+from app.telemetry import db_log_launch_telemetry, db_log_ping
 
 # create the FastAPI app instance
 app = FastAPI()
@@ -28,6 +29,10 @@ if not os.path.exists(trashcan_data_url):
 if not os.path.exists(traffic_data_url):
     os.makedirs(traffic_data_url)
 
+latest_city_map = None
+latest_trashcan_data = None
+latest_traffic_data = None
+
 
 # Setup Endpoints
 @app.post("/city_map")
@@ -35,11 +40,18 @@ def get_city_map(file: UploadFile = File(...)):
     # handle city map request
     if (file.filename.endswith(".json") == False):
         log_error("Invalid file type. Only JSON files are allowed.")
-        return {"error": "Invalid file type. Only JSON files are allowed."}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file type. Only JSON files are allowed.")
     
+    # Save the uploaded file to the specified directory
+    # file_path = uploads
     file_path = os.path.join(city_map_url, file.filename)
+    db_log_ping(file.filename, "city_map", 200)
     with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        shutil.copyfileobj(file.file, buffer)   
+
+    # Now reopen file and parse it and save it to the database
+    latest_city_map = file.filename
+    # keep record of latest file in the database
 
     log_info(f"File {file.filename} uploaded successfully to {city_map_url}.")
     return {"INFO": "File uploaded successfully"}
@@ -49,12 +61,19 @@ async def get_trashcan_data(file: UploadFile = File(...)):
     # handle trashcan data request
     if (file.filename.endswith(".csv") == False):
         log_error("Invalid file type. Only CSV files are allowed.")
-        return {"error": "Invalid file type. Only CSV files are allowed."}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file type. Only CSV files are allowed.")
     
     # Save the uploaded file to the specified directory
+    # file_path = uploads
     file_path = os.path.join(trashcan_data_url, file.filename)
+    db_log_ping(file.filename, "trashcan_data", 200)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+    # Now reopen file and parse it and save it to the database
+    latest_trashcan_data = file.filename
+    # keep record of latest file in the database
+
 
     log_info(f"File {file.filename} uploaded successfully to {trashcan_data_url}.")
     return {"INFO": "File uploaded successfully"}
@@ -64,11 +83,19 @@ def get_road_data(file: UploadFile = File(...)):
     # handle road data request
     if (file.filename.endswith(".csv") == False):
         log_error("Invalid file type. Only CSV files are allowed.")
-        return {"error": "Invalid file type. Only CSV files are allowed."}
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file type. Only CSV files are allowed.")
 
+    # Save the uploaded file to the specified directory
+    # file_path = uploads
     file_path = os.path.join(traffic_data_url, file.filename)
+    db_log_ping(file.filename, "road_data", 200)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+
+
+    # Now reopen file and parse it and save it to the database
+    latest_traffic_data = file.filename
+    # keep record of latest file in the database
 
     log_info(f"File {file.filename} uploaded successfully to {traffic_data_url}.")
     return {"INFO": "File uploaded successfully"}
@@ -76,10 +103,14 @@ def get_road_data(file: UploadFile = File(...)):
 @app.post("/train")  
 def train_model():
     # handle model training request
+    # Record entry for kicking in training model
+    db_log_launch_telemetry(latest_city_map, latest_trashcan_data, latest_traffic_data)
+    log_info("Training model with the latest data files.")
+
     # stop the current model, refresh the variables and start training
     # This is a placeholder for the actual training logic
 
-    return
+    return {"INFO": "Model training started."}
 
 @app.post("/predict")
 def predict_trashcan_status():
